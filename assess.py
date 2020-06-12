e6b343ca40b622b2be5a7fe1afdcd2493cf3955d
Chris Greer <chrisgreer88@gmail.com>
2020-06-12 10:32:15 -0700

    Pass in list bot ranges for CLI ladder assessment

--- assess.py
+++ assess.py
@@ -10,29 +10,30 @@ import settings
 
 def run_generation_ladder(
     environment_name,
-    species,
-    highest_generation,
+    species_list, # [(species, low_gen, high_gen), ...]
     num_workers=1,
 ):
-    num_entrants = 6
-    generations = [int(round(x)) for x in numpy.linspace(1, highest_generation, num_entrants)]
     bots = []
-    for i in generations:
-        Agent, agent_settings = configure_agent(environment_name, species, i, "evaluation")
-        bots.append(
-            Bot(
-                f"{species}-{i}",
-                Agent,
-                agent_settings,
+    for species, lowest_generation, highest_generation in species_list:
+        num_entrants = 6
+        generations = [int(round(x)) for x in numpy.linspace(lowest_generation, highest_generation, num_entrants)]
+        generations = list(set(generations))
+        for i in generations:
+            Agent, agent_settings = configure_agent(environment_name, species, i, "evaluation")
+            print(f"Adding bot {species}-{i} to tourney")
+            bots.append(
+                Bot(
+                    f"{species}-{i}",
+                    Agent,
+                    agent_settings,
+                )
             )
-        )
 
     env_class = get_env_module(environment_name)
     tournament = Tournament.setup(
         environment=env_class.Environment,
         bots=bots,
     )
-    # tournament.round_robin(num_games=200)
     for i in range(100):
         tournament.ladder(num_rounds=1, num_workers=num_workers)
         tournament.display_results()
@@ -76,9 +77,14 @@ def run_faceoff(
 
 if __name__ == "__main__":
     # run_faceoff(ENVIRONMENT, BOT_SPECIES, BOT_GENERATION)
+    species_list_str = sys.argv[2]
+    species_list = species_list_str.split(",")
+    species_list = [x.split("-") for x in species_list]
+    species_list = [(x[0], int(x[1].split("/")[0]), int(x[1].split("/")[1])) for x in species_list]
+    print(species_list)
+
     run_generation_ladder(
         environment_name=sys.argv[1],
-        species=sys.argv[2],
-        highest_generation=int(sys.argv[3]),
+        species_list=species_list,
         num_workers=settings.NUM_CORES,
     )
