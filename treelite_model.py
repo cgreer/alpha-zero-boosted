743ac7b434f3979b214fd843fa60807b2c15b61c
Chris Greer <chrisgreer88@gmail.com>
2020-06-23 06:40:59 -0700

    treelite model logging and threading

--- treelite_model.py
+++ treelite_model.py
@@ -13,6 +13,7 @@ def build_annotation_data(
     model,
     annotation_samples,
     output_path,
+    nthread=1,
 ):
     # sample_data_path in libsvm format
     samples_matrix = TreeliteDMatrix(annotation_samples)
@@ -20,6 +21,7 @@ def build_annotation_data(
     ann.annotate_branch(
         model=model,
         dmat=samples_matrix,
+        nthread=nthread,
         verbose=False,
     )
     ann.save(path=output_path)
@@ -33,22 +35,28 @@ def build_treelite_model(
     annotation_samples=None,
 ):
     # Load the model tree
+    print("Loading lightgbm model")
     treelite_model = TreeliteModel.load(
         gbdt_model_path,
         model_format=model_format,
     )
 
     # Build up branch expectations from data samples
+    print("Building branch expectations")
     annotation_results_path = None
     if annotation_samples is not None:
         tmp_annotation_info = f"{settings.TMP_DIRECTORY}/treelite_ann-{str(uuid4())}.info"
+        # XXX: :nthread doesn't seem to do anything and only ever utilizes 1
+        # core?
         annotation_results_path = build_annotation_data(
             treelite_model,
             annotation_samples,
             tmp_annotation_info,
+            nthread=12, # XXX: pass through
         )
 
     # Compile model to C/C++
+    print("Compiling Tree")
     params = dict(
         parallel_comp=settings.NUM_THREADS,
         # quantize=1, # Supposed to speed up predictions. Didn't when I tried it.
